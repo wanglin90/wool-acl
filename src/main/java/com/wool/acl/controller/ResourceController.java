20faac86f231d4ca1d21582b5e4bd0728967a97b
wanglin <wanglin20130410@126.com>
2017-02-17 17:46:20 +0800

    WOOL-20170217: add controller

---
+++ src/main/java/com/wool/acl/controller/ResourceController.java
@@ -0,0 +1,10 @@
+package com.wool.acl.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wanglin on 17-2-17.
+ */
+@RestController
+public class ResourceController {
+}
--- src/main/java/com/wool/acl/controller/page/WebPageController.java
+++ src/main/java/com/wool/acl/controller/page/WebPageController.java
@@ -1,4 +1,4 @@
-package com.wool.acl.controller;
+package com.wool.acl.controller.page;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
